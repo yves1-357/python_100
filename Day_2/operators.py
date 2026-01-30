97ad4fc4cadcd2a7f09a8e9a30df11447d605656
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2026-01-30 12:09:15 +0100

    operators

---
+++ Day_2/operators.py
@@ -0,0 +1,18 @@
+print("My age : " + str(25))
+print(6/2)
+print(6//2)
+print(3 + 3 - 3 + 3 - 3)
+
+height = 1.65 
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / (height ** 2)
+
+print(round(bmi))
+
+# height * 2 : C'est une multiplication (taille x 2).
+# height ** 2 : C'est un exposant (taille au carré, soit taille x taille).
+print(6 + 4 / 2 - (1 * 2))
+print(int("123") / int(12.3))
