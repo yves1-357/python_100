45d5a9d1c8cd7f5b96f92ef345b1d723700bc243
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2026-01-30 12:09:00 +0100

    dat_types

---
+++ Day_2/data_types.py
@@ -0,0 +1,19 @@
+print("Hello"[4])
+
+#integers
+print (1230+ 256)
+
+#float
+print( 3.5655)
+
+#boolean 
+print(True)
+print(False)
+print(len("244"))
+print(type("123"))
+print(type(123))
+print(type("3.78"))
+print(type(True))
+print(int("123") + int("123"))
+name = len(input("Enter your name : "))
+print(f"Number of letters in your name : {name}"   )
