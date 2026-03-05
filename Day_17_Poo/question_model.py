0a08d195916dd2fc4b52cbe24440a46529011c2d
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2026-03-05 18:47:12 +0100

    building class oop

---
+++ Day_17_Poo/question_model.py
@@ -0,0 +1,8 @@
+from data import question_data 
+class Question:
+    def __init__(self, question_texte, question_answer):
+        self.text = question_texte
+        self.answer = question_answer
+
+
+        
