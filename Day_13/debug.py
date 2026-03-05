e1400a0fb083cb6d6d0e9e2e23c450f0d7837efe
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2026-03-05 18:41:23 +0100

    fizzbuzz debug

---
+++ Day_13/debug.py
@@ -0,0 +1,28 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+print(is_leap(2000))
+
+
+# Target is the number up to which we count
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+            
+fizz_buzz(12)
