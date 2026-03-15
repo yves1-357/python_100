bbd89778ed116228c4350fb874db9b88ddf0c9b6
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2026-03-15 05:03:25 +0100

    logique_metier du jeu snake

---
+++ Day_20_snake_game/snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+Starting_Positions = [(0, 0), (-20, 0), (-40, 0)]
+Move_distance = 15
+is_game_on = True
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in Starting_Positions:
+            self.add_segment(position)
+
+    def move_snake(self):
+        for i in range(len(self.segments) - 1, 0 , -1):
+            new_x = self.segments[i - 1].xcor()
+            new_y = self.segments[i - 1].ycor()
+            self.segments[i].goto(new_x, new_y)
+        self.segments[0].forward(Move_distance)
+
+    def up(self):
+        if self.segments[0].heading() != 270:
+            self.segments[0].setheading(90)
+    def down(self):
+        if self.segments[0].heading() != 90:
+            self.segments[0].setheading(270)
+    def left(self):
+        if self.segments[0].heading() != 0:
+            self.segments[0].setheading(180)
+    def right(self):
+        if self.segments[0].heading() != 180:
+            self.segments[0].setheading(0)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+     
